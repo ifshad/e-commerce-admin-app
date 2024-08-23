4148cefa115b140a21a5514dbea54dc426150b5a
ifshad <shoronshark@gmail.com>
2024-08-23 19:11:14 +0600

    error in product details page for single product

---
+++ app/services/getOneProduct.ts
@@ -0,0 +1,23 @@
+import { Product } from "../products/page";
+import { fetchProducts } from "./productService";
+
+export default async function getOneProduct(
+  id: number
+): Promise<Product | null> {
+  try {
+    const data = await fetchProducts();
+
+    const product = data.products.find((product: Product) => product.id === id);
+
+    if (!product) {
+      return null; 
+    }
+
+    return product;
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    return null; 
+  }
+}
+
+export const revalidate = 1;
--- app/services/productService.ts
+++ app/services/productService.ts
@@ -3,9 +3,10 @@ import apiClient from "./apiClient";
 export const fetchProducts = async () => {
   try {
     const response = await apiClient.get("/products.json");
+    console.log("Fetched Products:", response.data);
     return response.data;
   } catch (error) {
     console.error("Error fetching products:", error);
-    return { products: [] }; // Return an empty array if there is an error
+    return { products: [] }; 
   }
 };
